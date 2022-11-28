5c6438997b9ebf08507fbb0bc27ad2d097dd9d6f
Efoneli <abbaefoneli@gmail.com>
2022-11-29 03:21:27 +1030

    portfolio features

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+import imageGal from '../assets/imageGal.jpeg'
+import todo from '../assets/todo.jpeg'
+import signup from '../assets/signup.jpeg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:imageGal,
+        github:"https://github.com/Efoneli/Image-Gallery-Usiing-Unspalsh-APi",
+        live:"https://playful-concha-b169db.netlify.app/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:todo,
+        github:"https://github.com/Efoneli/ToDo-App-with-Local-Storage",
+        live:"https://flourishing-beijinho-feb335.netlify.app/",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:signup,
+        github:"https://github.com/Efoneli/countdown-timer",
+        live:"https://countdown-timer-eta-nine.vercel.app/",
+    },
+    // {
+       // id:4,
+       // name:"React JS Application",
+       // image:RealEst,
+       // github:"",
+       // live:"",
+   // },
+
+
+]
