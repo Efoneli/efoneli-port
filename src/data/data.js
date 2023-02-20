2a7899bbe64db85882f5a780ce90377056b7461f
Efoneli <abbaefoneli@gmail.com>
2023-02-20 02:57:46 +0100

    new proj

--- src/data/data.js
+++ src/data/data.js
@@ -1,19 +1,28 @@
-import imageGal from '../assets/imageGal.jpeg'
-import todo from '../assets/todo.jpeg'
-import signup from '../assets/signup.jpeg'
-import starfinder from '../assets/starfinder.jpeg'
+import imageGal from '../assets/imageGal.jpeg';
+import todo from '../assets/todo.jpeg';
+import signup from '../assets/signup.jpeg';
+import starfinder from '../assets/starfinder.jpeg';
+import meta from '../assets/meta.jpeg';
+import movie from '../assets/movie-app.jpeg';
 
 
 export const data=[
     {
         id:1,
         name:"React JS Application",
+        image:movie,
+        github:"https://github.com/Efoneli/movie-app.git",
+        live:"https://movie-app-efoneli.vercel.app/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
         image:imageGal,
         github:"https://github.com/Efoneli/Image-Gallery-Usiing-Unspalsh-APi",
         live:"https://playful-concha-b169db.netlify.app/",
     },
     {
-        id:2,
+        id:3,
         name:"React JS Application",
         image:todo,
         github:"https://github.com/Efoneli/ToDo-App-with-Local-Storage",
@@ -29,11 +38,18 @@ export const data=[
     },
 
     {
-        id:3,
+        id:5,
         name:"React JS Application",
         image:signup,
         github:"https://github.com/Efoneli/countdown-timer",
         live:"https://countdown-timer-eta-nine.vercel.app/signup",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:meta,
+        github:"https://github.com/Efoneli/meta-task",
+        live:"https://meta-task-chi.vercel.app/",
     },
 
     
