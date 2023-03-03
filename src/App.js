7323a2126084a17effa1ee1d4f97aaef934bb8af
Efoneli <abbaefoneli@gmail.com>
2023-03-03 20:35:54 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ function App() {
   return (
     <div className="flex flex-col">
       <Navbar />
-      <div className="">
+      <div className="mt-10 pt-10">
       {/* my-20 py-20 */}
         <Home />
       </div>
-      <div className="">
+      <div className=" border-t-2 border-gray-400">
         <About />
       </div>
-      <div className="">
+      <div className=" border-t-2 border-gray-400">
         <Skills />
       </div>
-      <div className="">
+      <div className=" border-t-2 border-gray-400">
         <Work />
       </div>
-      <div className="">
+      <div className=" border-t-2 border-gray-400">
         <Contact />
       </div>
     </div>
