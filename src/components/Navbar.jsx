f0061cfbae3643aafd1fb114522dc2ab679ae718
Efoneli <abbaefoneli@gmail.com>
2023-03-03 23:32:17 +0100

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,10 +92,48 @@ const Navbar = () => {
             Contact
           </Link>
         </li>
+
+        <div className=' lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://www.linkedin.com/in/abba-efoneli-082a421b6/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://github.com/efoneli'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://efonelisarah@gmail.com
+              '
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li> */}
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://eu.docworkspace.com/d/sIILBtN-9Ac7Bk5wG?sa=e1&st=0'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
       </ul>
 
       {/* Social icons */}
-      <div className=' lg:flex fixed flex-col top-[35%] left-0'>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
