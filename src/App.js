43211aa4fc136661ff05fa7a2e33975a7b0f96da
Efoneli <abbaefoneli@gmail.com>
2023-03-03 20:01:30 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 
-
 function App() {
   return (
-    <div className="h-screen">
+    <div className="flex flex-col gap-10">
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
+      <div className="my-20 py-20">
+        <Home />
+      </div>
+      <div>
+        <About />
+      </div>
+      <div className="mb-50">
+        <Skills />
+      </div>
+      <div className="py-20">
+        <Work />
+      </div>
+      <div>
+        <Contact />
+      </div>
     </div>
   );
 }
