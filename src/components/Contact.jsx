892e7612f7ab916d79482acfb60ce1be8eb4cfbd
efoneli <abbaefoneli@gmail.com>
2024-11-28 13:06:11 +0100

    new work added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,79 @@
 import React from 'react'
 
 const Contact = () => {
+  function Submit(e) {
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
+    fetch(
+      "https://script.google.com/macros/s/AKfycbyg5LiO5aMlaYfsGqNSND73p5FPvfq7gu7ntMnf5iIJmgQqux6y-lN54zodUS-jaWaEqw/exec",
+      {
+        method: "POST",
+        body: formDatab
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div name='contact' className='w-full bg-[#0a192f] flex justify-center items-center'>
-        <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
+        <form onSubmit={(e) => Submit(e)} className='form flex flex-col max-w-[600px] w-full'>
             <div className='py-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or send me an email -<span className='font-2xl text-pink-600 cursor-pointer hover:text-pink-400'> efonelisarah@gmail.com</span></p>
             </div>
-            <input className='bg-[#ccd6f6] p-2 rounded-lg' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6] rounded-lg' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2 rounded-lg' name="message" rows="10" placeholder='Message'>
+            <input className='bg-[#ccd6f6] p-2 rounded-lg' type="text" placeholder='Name' name='Name' />
+            <input className='my-4 p-2 bg-[#ccd6f6] rounded-lg' type="email" placeholder='Email' name='Email' />
+            <textarea className='bg-[#ccd6f6] p-2 rounded-lg' name="Message" rows="5" placeholder='Message'>
             </textarea>
-            <button className='text-gray-300 border-2 border-pink-600 rounded-lg w-full text-center hover:bg-pink-600 hover:border-gray-200 px-4 py-3 my-8 mx-auto flex items-center'>Hire Me</button>
+            <button className='text-gray-300 border-2 border-pink-600 rounded-lg w-full text-center hover:bg-pink-600 hover:border-gray-200 px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
         </form>
     </div>
   )
 }
 
 export default Contact
+
+
+// export default function App() {
+//   function Submit(e) {
+//     const formEle = document.querySelector("form");
+//     const formDatab = new FormData(formEle);
+//     fetch(
+//       "https://script.google.com/macros/s/AKfycbxUX4hu2QEQ7r9TwrSo9vAHydDXv4VwtEDUacCifXaEecIH2bWHQSnFWtd9NdNIUVGDdQ/exec",
+//       {
+//         method: "POST",
+//         body: formDatab
+//       }
+//     )
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+//   return (
+//     <div className="App">
+//       <h1>Contact Me form</h1>
+//       <h2>
+//         This demonstrates how to send data from a website form to Google sheet
+//         in React or Vanilla jS
+//       </h2>
+//       <div>
+//         <form className="form" onSubmit={(e) => Submit(e)}>
+//           <input placeholder="Your Name" name="Name" type="text" />
+//           <input placeholder="Your Email" name="Email" type="text" />
+//           <input placeholder="Your Message" name="Message" type="text" />
+//           <input name="Name" type="submit" />
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
--- src/data/data.js
+++ src/data/data.js
@@ -1,19 +1,11 @@
 import imageGal from '../assets/imageGal.jpeg';
 import todo from '../assets/todo.jpeg';
-import signup from '../assets/signup.jpeg';
-import starfinder from '../assets/starfinder.jpeg';
-import meta from '../assets/meta.jpeg';
 import movie from '../assets/movie-app.jpeg';
-
+import tele from '../assets/tele.jpeg';
+import food from '../assets/food.jpeg';
+import resort from '../assets/resort.jpeg';
 
 export const data=[
-    {
-        id:1,
-        name:"React JS Application",
-        image:movie,
-        github:"https://github.com/Efoneli/movie-app.git",
-        live:"https://movie-app-efoneli.vercel.app/",
-    },
     {
         id:2,
         name:"JS Application",
@@ -22,43 +14,55 @@ export const data=[
         live:"https://playful-concha-b169db.netlify.app/",
     },
     {
+        id:1,
+        name:"React JS Application",
+        image:movie,
+        github:"https://github.com/Efoneli/movie-app.git",
+        live:"https://movie-app-efoneli.vercel.app/",
+    },
+    {
         id:3,
         name:"JS Application",
         image:todo,
         github:"https://github.com/Efoneli/ToDo-App-with-Local-Storage",
         live:"https://flourishing-beijinho-feb335.netlify.app/",
     },
-
+    // {
+    //     id:5,
+    //     name:"React JS Application",
+    //     image:signup,
+    //     github:"https://github.com/Efoneli/countdown-timer",
+    //     live:"https://countdown-timer-eta-nine.vercel.app/signup",
+    // },
+    // {
+    //     id:6,
+    //     name:"React JS Application",
+    //     image:meta,
+    //     github:"https://github.com/Efoneli/meta-task",
+    //     live:"https://meta-task-chi.vercel.app/",
+    // },
     {
-        id:4,
-        name:"React JS Application",
-        image:starfinder,
-        github:"https://github.com/workshopapps/vipcustomer.web",
-        live:"https://starfinder.dev/",
-    },
-
-    {
-        id:5,
-        name:"React JS Application",
-        image:signup,
-        github:"https://github.com/Efoneli/countdown-timer",
-        live:"https://countdown-timer-eta-nine.vercel.app/signup",
-    },
+        id:6,
+        name:"Next JS Application",
+        image:tele,
+        github:"https://github.com/Efoneli/careHub.git",
+        live:"https://telecare-hub.vercel.app/",
+    }, 
     {
         id:6,
         name:"React JS Application",
-        image:meta,
-        github:"https://github.com/Efoneli/meta-task",
-        live:"https://meta-task-chi.vercel.app/",
-    },
+        image:food,
+        github:"https://github.com/Efoneli/careHub.git",
+        live:"https://telecare-hub.vercel.app/",
+    }, 
     {
         id:6,
         name:"React JS Application",
-        image:meta,
-        github:"https://github.com/Efoneli/meta-task",
-        live:"https://meta-task-chi.vercel.app/",
-    },
-    
+        image:resort,
+        github:"https://github.com/Efoneli/careHub.git",
+        live:"https://telecare-hub.vercel.app/",
+    }, 
+            
 
 
 ]
